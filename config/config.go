dbf7947e215a66912e46d8acee8c7486bf67b4ba
AntonyChR <antonychr99@gmail.com>
2023-06-24 10:36:31 -0500

    [add] config managment

---
+++ config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/AntonyChR/terminalGPT/color"
+)
+
+type Credentials struct {
+	Apikey string `json:"api_key"`
+}
+
+var userHomeDir, _ = os.UserHomeDir()
+
+var CONFIG_PATH_DIR string = userHomeDir + "/.config/gpt"
+var CONFIG_FILE_NAME string = "/gpt.dat"
+
+var CONFIG_FILE_PATH string = path.Join(CONFIG_PATH_DIR, CONFIG_FILE_NAME)
+
+func (c *Credentials) Save() {
+	if _, err := os.Stat(CONFIG_PATH_DIR + CONFIG_FILE_NAME); err == nil {
+		fmt.Println(color.Yellow("[i] A configuration file already exists, it will be overwritten with the new credentials"))
+	}
+	os.Mkdir(CONFIG_PATH_DIR, os.ModePerm)
+	file, _ := os.Create(CONFIG_PATH_DIR + CONFIG_FILE_NAME)
+	jsonBytes, _ := json.Marshal(c)
+	file.WriteString(string(jsonBytes))
+	fmt.Println(color.Green("[] The credentials have been saved successfully"))
+}
+
+func (c *Credentials) Get() error {
+	if _, err := os.Stat(CONFIG_PATH_DIR + CONFIG_FILE_NAME); err != nil {
+		err := errors.New(color.Red("[] the configuration file does not exist,use the -c flag to add the credentials"))
+		return err
+	}
+
+	file, _ := os.ReadFile(CONFIG_PATH_DIR + CONFIG_FILE_NAME)
+
+	err := json.Unmarshal(file, &c)
+
+	return err
+}
+
+func (c *Credentials) Exist() bool {
+	if c.Apikey != "" {
+		return true
+	}
+	return false
+}
