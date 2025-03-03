f1c8edaa13e44466ea7bea0cc8f7b179382a403d
AntonyChR <antonychr99@gmail.com>
2025-03-03 16:47:00 -0500

    refactor main: enhance configuration handling and add file input support

--- main.go
+++ main.go
@@ -1,60 +1,179 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
+	"path"
 
 	colors "github.com/AntonyChR/terminalGPT/colors"
-	config "github.com/AntonyChR/terminalGPT/config"
+	configService "github.com/AntonyChR/terminalGPT/config"
 	openaiservice "github.com/AntonyChR/terminalGPT/openai_service"
 	userinterface "github.com/AntonyChR/terminalGPT/user_interface"
+	utils "github.com/AntonyChR/terminalGPT/utils"
 )
 
+const (
+	CONFIG_FILE_JSON string = "/config.json"
+	LOG_FILE         string = "/logs.txt"
+)
+
+var (
+	// paths
+	CONFIG_DIR        string = path.Join(HOME, ".config/gpt")
+	CONVERSATIONS_DIR string = path.Join(HOME, "/conversations")
+	HOME, _                  = os.UserHomeDir()
+	CURRENT_DATE      string = utils.GetCurrentDate()
+
+	// flags
+	lang = flag.String("lang", "es", "-lang <es,en>")
+	file = flag.String("file", "", "-file <file_name>")
+)
+
+type FileTitle struct {
+	Title    string `json:"title"`
+	FileName string `json:"file_name"`
+}
+
 func main() {
+	flag.Parse()
 
-	var credentials config.Credentials
-	if err := credentials.Get(); err != nil {
-		err = credentials.PromptUserForCredentials()
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-		credentials.Save()
-	}
-
-	config := openaiservice.ApiConfiguration{
-		Apikey: credentials.Apikey,
-		Model:  "gpt-3.5-turbo",
-		ApiUrl: "https://api.openai.com/v1/chat/completions",
-	}
-	userInterface := userinterface.NewUserInterface()
-	chat := openaiservice.NewChat(config)
-
-	var input string
-	var err error
-
+	userInterface := userinterface.New()
 	go userInterface.PrintChannelMessages()
 
-	for {
-		input, err = userInterface.GetInput(colors.Colorize("yellow", "[A]: "))
+	configFile := path.Join(CONFIG_DIR, CONFIG_FILE_JSON)
 
-		if err != nil {
-			fmt.Println(err.Error())
-			continue
-		}
+	config, err := configService.LoadConfig(configFile)
 
-		if input == "reset\n" {
-			chat.Reset()
-			continue
-		}
+	if err != nil {
+		handleError(err, "Error loading config file")
+		config, err = createNewConfig(userInterface)
+		handleError(err, "Error creating new config file")
 
-		chat.AddMessageAsUser(input)
-		err = chat.GetStreamCompletion(userInterface.PrintChannel)
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
+		err = configService.SaveNewConfig(config, CONFIG_DIR, "config.json")
+		handleError(err, "Error saving new config file")
+	}
+
+	if *lang != "" && *lang != config.Lang {
+		println("Changing language to " + *lang)
+		config.Lang = *lang
+		err = configService.SaveNewConfig(config, CONFIG_DIR, "config.json")
+		handleError(err, "Error saving new config file")
+	}
+
+	fileContent := ""
+	if *file != "" {
+		//read content as string
+		content, err := os.ReadFile(*file)
+		handleError(err, "Error reading file")
+		fileContent = string(content)
+
+		fmt.Printf("The content of the file '%s' will be concatenated to the prompt\n", *file)
 
 	}
 
+	sessionConfig := openaiservice.ApiConfiguration{
+		Apikey:        config.ApiKey,
+		ApiUrl:        "https://api.deepseek.com/chat/completions",
+		Model:         "deepseek-chat",
+		InitialPrompt: config.InitialPrompt,
+	}
+
+	chat := openaiservice.NewChat(sessionConfig)
+
+	for {
+		input, err := userInterface.GetInput(colors.Yellow("[A]: "))
+
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		switch input {
+
+		case "reset":
+			chat.Reset()
+			continue
+		case "save":
+			if config.Lang == configService.SPANISH {
+				chat.AddMessageAsUser(openaiservice.PROMPT_TO_GET_TITLE_ES)
+			} else {
+				chat.AddMessageAsUser(openaiservice.PROMPT_TO_GET_TITLE_EN)
+			}
+			println("Generating title...")
+			comp, err := chat.GetCompletion()
+			handleError(err, "Error getting completion")
+
+			// remove completion prompt
+			chat.RemoveLastMessage()
+
+			comp = utils.FixJsonString(comp)
+
+			var fileTitle FileTitle
+
+			err = json.Unmarshal([]byte(comp), &fileTitle)
+			handleError(err, "Error unmarshalling json")
+
+			err = chat.Save(fileTitle.Title, fileTitle.FileName, CONVERSATIONS_DIR)
+			handleError(err, "Error saving conversation")
+
+			fmt.Println(colors.Green("Conversation saved successfully in " + CONVERSATIONS_DIR + "/" + fileTitle.FileName))
+
+			// remove info generated
+			chat.RemoveLastMessage()
+			continue
+
+		case "exit":
+			os.Exit(0)
+		}
+
+		chat.AddMessageAsUser(input + "\n" + fileContent)
+		err = chat.GetStreamCompletion(userInterface.PrintChannel)
+		handleError(err, "Error getting completion")
+	}
+
+}
+
+func createNewConfig(userInt *userinterface.UserInterfaceIO) (*configService.Config, error) {
+	userInt.PrintChannel <- colors.Yellow("Please enter your API key: ")
+	api_key, err := userInt.GetInput(colors.Yellow(">: "))
+	if err != nil {
+		println(colors.Red("Error getting input"))
+		os.Exit(1)
+	}
+
+	newConf := &configService.Config{
+		ApiKey: api_key,
+	}
+
+	lang, err := userInt.GetInput(colors.Yellow("Language (es/en): "))
+
+	if err != nil {
+		println(colors.Red("Error getting input"))
+		os.Exit(1)
+	}
+
+	if lang != configService.SPANISH && lang != configService.ENGLISH {
+		println(colors.Red("Invalid language, spanish will be set by default"))
+		lang = configService.SPANISH
+	}
+
+	newConf.Lang = lang
+
+	newConf.InitialPrompt = "You are an helpful assistant"
+
+	newConf.InitialPrompt = newConf.InitialPrompt + ", the current date is " + CURRENT_DATE
+	if err != nil {
+		return nil, err
+	}
+
+	return newConf, nil
+}
+
+func handleError(err error, message string) {
+	if err != nil {
+		fmt.Printf("%s: %v\n", message, err)
+		os.Exit(1)
+	}
 }
--- openai_service/openai.go
+++ openai_service/openai.go
@@ -7,11 +7,13 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path"
 	"regexp"
 	"strings"
 
 	color "github.com/AntonyChR/terminalGPT/colors"
-	customErrors "github.com/AntonyChR/terminalGPT/customErrors"
+	"github.com/AntonyChR/terminalGPT/logs"
 )
 
 var ChatRoles = Roles{
@@ -20,17 +22,44 @@ var ChatRoles = Roles{
 	User:      "user",
 }
 
+// prompt to generate title, file name with title plus txt extension, this will be generated in json format
+var PROMPT_TO_GET_TITLE_EN string = `Generate a title for the current conversation and a corresponding file name with a .md extension. 
+The file name must be the same as the title, replacing spaces with underscores. The title should be descriptive and concise, written in English. 
+The response should be generated in JSON format in one line.
+
+Example:
+Title: "My first conversation"
+File name: "My_first_conversation.md"
+
+the response looks like this:
+{"title": "My first conversation","file_name": "My_first_conversation.md"}
+`
+
+var PROMPT_TO_GET_TITLE_ES string = `Genera un título para la conversación actual y un nombre de archivo correspondiente con una extensión .md.
+El nombre del archivo debe ser igual al título, reemplazando los espacios con guiones bajos. El título debe ser descriptivo y conciso.
+La respuesta debe generarse en formato JSON en una sola línea.
+
+Ejemplo:
+Título: "Mi primera conversación"
+Nombre de archivo: "Mi_primera_conversación.md"
+
+la repsuesta que generas debería verse así:
+{"title": "Mi primera conversación","file_name": "Mi_primera_conversación.md"}
+`
+
 type ApiConfiguration struct {
-	Apikey      string
-	Model       string
-	ApiUrl      string
-	Temperature float32 //default 0.0
+	Apikey        string
+	Model         string
+	ApiUrl        string
+	InitialPrompt string
+	Temperature   float32 //default 0.0
+	Stream        bool    //default false
 }
 
 func NewChat(c ApiConfiguration) *Openai {
 	initialMessage := Message{
 		Role:    ChatRoles.System,
-		Content: "You are an assistant",
+		Content: c.InitialPrompt,
 	}
 	return &Openai{
 		apikey: c.Apikey,
@@ -41,15 +70,16 @@ func NewChat(c ApiConfiguration) *Openai {
 				initialMessage,
 			},
 			Temperature: c.Temperature,
-			Stream:      true,
+			Stream:      c.Stream,
 		},
 	}
 }
 
 type Openai struct {
-	apikey string
-	url    string
-	Chat   Chat
+	apikey     string
+	url        string
+	Chat       Chat
+	LogChannel chan string
 }
 
 type Chat struct {
@@ -59,55 +89,107 @@ type Chat struct {
 	Stream      bool      `json:"stream,omitempty"`
 }
 
-func (o *Openai) appendMessageToList(m Message) {
-	o.Chat.Messages = append(o.Chat.Messages, m)
+func (c *Chat) appendMessage(m Message) {
+	c.Messages = append(c.Messages, m)
+
 }
 
 func (o *Openai) AddMessageAsUser(input string) {
-	o.appendMessageToList(Message{Role: ChatRoles.User, Content: input})
+	o.Chat.appendMessage(Message{Role: ChatRoles.User, Content: input})
 }
 
-func (o *Openai) GetStreamCompletion(streamChannel chan string) error {
+func (o *Openai) AddMessageAsAssistant(input string) {
+	o.Chat.appendMessage(Message{Role: ChatRoles.Assistant, Content: input})
+}
+
+func (o *Openai) CreateRequest() (*http.Request, error) {
 	encodedData, _ := json.Marshal(o.Chat)
 	req, err := http.NewRequest("POST", o.url, bytes.NewBuffer(encodedData))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	req.Header.Set("Authorization", "Bearer "+o.apikey)
 	req.Header.Set("Content-Type", "application/json")
+	return req, nil
+
+}
+
+func (o *Openai) RemoveLastMessage() {
+	o.Chat.Messages = o.Chat.Messages[:len(o.Chat.Messages)-1]
+}
+
+func (o *Openai) GetCompletion() (string, error) {
+	o.Chat.Stream = false
+	req, err := o.CreateRequest()
+	if err != nil {
+		return "", err
+	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		o.LogChannel <- createRequestErrorEvent(resp, err, "")
+		return "", err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		if resp.StatusCode == 429 {
-			return &customErrors.RequestError{StatusCode: resp.StatusCode, Err: errors.New("Request limit exceeded")}
+			o.LogChannel <- createRequestErrorEvent(resp, err, "Request limit exceeded")
+			panic("Request limit exceeded")
 		}
-		return &customErrors.RequestError{StatusCode: resp.StatusCode, Err: errors.New("unespected request error")}
+		o.LogChannel <- createRequestErrorEvent(resp, err, "")
+		panic("unespected request error")
+	}
+
+	var completionResponse CompletionResponse
+	bodyBytes, _ := io.ReadAll(resp.Body)
+	err = json.Unmarshal(bodyBytes, &completionResponse)
+	if err != nil {
+		return "", fmt.Errorf("error Unmarshal: %v", err)
+	}
+
+	return completionResponse.Choices[0].Message.Content, nil
+
+}
+
+// get completion with stream
+func (o *Openai) GetStreamCompletion(stringChannel chan string) error {
+	o.Chat.Stream = true
+	req, _ := o.CreateRequest()
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return errors.New("error making request " + err.Error())
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode == 429 {
+			panic("Request limit exceeded")
+		}
+		panic("unespected request error")
 	}
 
 	chunkBuffer := make([]byte, 4096)
 	re := regexp.MustCompile(`(?s)"finish_reason":(null|"stop").*`)
 	msg := Message{Role: ChatRoles.Assistant}
 	content := ""
-	fmt.Printf("\n")
 	for {
 		chunkSize, err := resp.Body.Read(chunkBuffer)
 		if err != nil {
 			if errors.Is(err, io.EOF) {
-				streamChannel <- "\n\n"
+				stringChannel <- "\n\n"
 				break
 			}
-			fmt.Println("ERROR: ", err.Error())
+			logStr := "error reading stream completion chunk " + err.Error()
+			fmt.Println(logStr)
+			o.LogChannel <- logs.NewEvent(logs.ErrorLevel, logStr, logs.GetDate()).ToStr()
 		}
 
 		chunkString := string(chunkBuffer[:chunkSize])
-
-		sep := `data: {"id":"chatcmpl`
+		sep := `data: {"id":"`
 
 		splitedChunkString := strings.Split(chunkString, sep)
 
@@ -116,26 +198,65 @@ func (o *Openai) GetStreamCompletion(streamChannel chan string) error {
 				continue
 			}
 			var chunkObject CompletionResponse
-			validJson := `{"id":"chatcmpl` + re.ReplaceAllString(str, `"finish_reason":"stop"}]}`)
+			validJson := `{"id":"` + re.ReplaceAllString(str, `"finish_reason":"stop"}]}`)
 			err := json.Unmarshal([]byte(validJson), &chunkObject)
 			if err != nil {
-				fmt.Println(color.Colorize("red", err.Error()))
+				logStr := "error unmarshalling json del stream completion chunk " + err.Error()
+				fmt.Print(color.Red(logStr))
+				o.LogChannel <- logs.NewEvent(logs.ErrorLevel, logStr, logs.GetDate()).ToStr()
 				continue
 			}
 			deltaString := chunkObject.Choices[0].Delta.Content
 			content += deltaString
-			streamChannel <- color.Colorize("green", deltaString) 
+			stringChannel <- color.Green(deltaString)
 		}
 
 	}
 
 	msg.Content = content
-	o.appendMessageToList(msg)
+	o.Chat.appendMessage(msg)
 	return nil
 
 }
 
 func (o *Openai) Reset() {
-	fmt.Println(color.Colorize("red", "Reset context"))
+	fmt.Println(color.Red("Reset context"))
 	o.Chat.Messages = []Message{o.Chat.Messages[0]}
+}
+
+// save chat as md file
+func (o *Openai) Save(title, fileName, dir string) error {
+
+	//check if the route exists
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.Mkdir(dir, 0755)
+	}
+
+	route := path.Join(dir, fileName)
+
+	file, err := os.Create(route)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	file.WriteString("# Title: " + title + "\n\n")
+
+	for _, message := range o.Chat.Messages {
+		if message.Role == ChatRoles.User {
+			file.WriteString("### [A] " + message.Content + "\n\n---\n\n")
+		}
+		if message.Role == ChatRoles.Assistant {
+			file.WriteString(message.Content)
+		}
+	}
+	return nil
+
+}
+
+func createRequestErrorEvent(resp *http.Response, err error, additionalInfo string) string {
+	bodyBytes, _ := io.ReadAll(resp.Body)
+	logMsg := fmt.Sprintf("error making request\n%s \n%s\nResponse body: %s\nStatus code: %d", additionalInfo, err.Error(), string(bodyBytes), resp.StatusCode)
+	event := logs.NewEvent(logs.ErrorLevel, logMsg, logs.GetDate())
+	return event.ToStr()
 }
